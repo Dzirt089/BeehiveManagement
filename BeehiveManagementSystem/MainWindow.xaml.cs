7da3187107f263b799b72c2d1701db0cf9c75efa
Илья Емец <67930111+Dzirt089@users.noreply.github.com>
2022-07-06 20:16:35 +0300

    использовали интерфейс для добавления связывания данных в приложение WPF

--- BeehiveManagementSystem/MainWindow.xaml.cs
+++ BeehiveManagementSystem/MainWindow.xaml.cs
@@ -21,17 +21,18 @@ namespace BeehiveManagementSystem
     /// </summary>
     public partial class MainWindow : Window
     {
-        private Queen queen = new Queen();
+        private readonly Queen queen;
         private DispatcherTimer timer = new DispatcherTimer();
 
 
         public MainWindow()
         {
-            InitializeComponent();          
-            statusReport.Text = queen.StatusReport;
-            //timer.Tick += Timer_Tick;
-            //timer.Interval = TimeSpan.FromSeconds(1.5);
-            //timer.Start();
+            InitializeComponent();
+            queen = Resources["queen"] as Queen;
+            //statusReport.Text = queen.StatusReport;
+            timer.Tick += Timer_Tick;
+            timer.Interval = TimeSpan.FromSeconds(1.5);
+            timer.Start();
         }
 
         private void Timer_Tick(object sender, EventArgs e)
@@ -42,13 +43,13 @@ namespace BeehiveManagementSystem
         private void WorkShift_Click(object sender, RoutedEventArgs e)
         {
             queen.WorkTheNextShift();
-            statusReport.Text = queen.StatusReport;
+            //statusReport.Text = queen.StatusReport;
         }
 
         private void AssignJob_Click(object sender, RoutedEventArgs e)
         {
             queen.AssignBee(jobSelector.Text);
-            statusReport.Text = queen.StatusReport;
+            //statusReport.Text = queen.StatusReport;
         }
     }
 }
